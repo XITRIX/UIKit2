d16b39faaee5dcd9c0362b9c5be43b17c791c133
Daniil Vinogradov <xitrix@bk.ru>
2023-09-06 23:33:55 +0200

    Semi working

---
+++ demo/main.cpp
@@ -0,0 +1,6 @@
+#include <Application.hpp>
+
+int main() {
+    Application::test();
+    return 0;
+}
---
+++ library/include/Application.hpp
@@ -0,0 +1,10 @@
+//
+// Created by Даниил Виноградов on 06.09.2023.
+//
+
+#pragma once
+
+class Application {
+public:
+    static void test();
+};
